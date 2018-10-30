cffd4c7ffeff17fc68ff3a303fb3675fccf90c74
Hamish Dickson <hamish.dickson96@gmail.com>
2018-10-30 17:38:45 +1100

    Enemy movement working on instance spawner script created nothing in it

---
+++ StrikeOne/Assets/ScriptBois/EnemyMove.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    //Attack
+    public float stopRange;
+    private Transform target;
+    public float moveSpeed = 1f;
+    private Rigidbody rb;
+    private bool move = true;
+    // Use this for initialization
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("PlayerTarget").transform;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float distToTarg = Vector3.Distance(transform.position, target.position);
+        if (move)
+        {
+            transform.LookAt(target);
+            rb.velocity = transform.forward * moveSpeed;
+        }
+        if (distToTarg < stopRange)
+        {
+            move = false;
+        }
+        else
+        {
+            move = true;
+        }
+
+    }
+}
