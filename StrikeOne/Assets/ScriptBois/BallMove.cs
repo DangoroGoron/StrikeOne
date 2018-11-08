5b8e4bd44399c6bcce85c70a9b8eb34771c5e272
Hamish Dickson <hamish.dickson96@gmail.com>
2018-11-08 13:15:30 +1100

    Ball movement implemented

--- StrikeOne/Assets/ScriptBois/BallMove.cs
+++ StrikeOne/Assets/ScriptBois/BallMove.cs
@@ -6,14 +6,23 @@ public class BallMove : MonoBehaviour
 {
     public float ballSpeed;
     public float speedInc;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        
+    private bool ballMove;
+    private Rigidbody rb;
+
+    private Material mat;
+
+    private void Awake()
+    {
+        mat = GetComponent<Renderer>().material;
+        rb = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update () {
+        if (ballMove)
+        {
+            mat.color = new Color(Random.value, Random.value, Random.value);
+            rb.velocity = transform.forward * ballSpeed;
+        }
     }
 
     float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
@@ -24,13 +33,21 @@ public class BallMove : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
+        if (!other.CompareTag("PlayerTarget"))
+            return;
         if (Input.GetMouseButtonDown(0))
         {
             {
                 //points to the mouse's position when the left mouse button is clicked
                 Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                Debug.Log(Input.mousePosition);
-                
+                RaycastHit hit;
+                Physics.Raycast(ray, out hit);
+                Debug.Log(hit.point);
+                Vector3 point = hit.point;
+                point.y = transform.position.y;
+                transform.LookAt(point);
+                ballMove = true;
+                ballSpeed += speedInc;
             }
         }
 
